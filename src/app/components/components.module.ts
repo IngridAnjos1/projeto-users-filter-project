f530d383861ac32eeb8709cbaa66dd10688084e5
Ingrid Anjos <ianjos@ctiglobal.com>
2025-02-28 14:03:08 -0300

    first commit

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { NgModel } from "@angular/forms";
+
+@NgModule({
+    declarations:[],
+    imports:[],
+    exports:[],
+    
+})
+export class ComponentsModule{
+
+}
