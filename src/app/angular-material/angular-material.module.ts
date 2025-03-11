5b1a1ce7f25fa267948e493e336e798d5baf263d
Ingrid Anjos <ianjos@ctiglobal.com>
2025-03-11 10:13:57 -0300

    feat: import adjustment in angular-material and components module

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,11 +1,40 @@
 import { NgModule } from "@angular/core";
 
-@NgModule({
-    declarations:[],
-    imports:[
-    ],
-    exports:[]
-})
-export class AngularMaterialModule{
+import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from "@angular/material/core";
 
-}
+
+@NgModule({
+    declarations: [],
+    imports: [
+        MatListModule,
+        MatTableModule,
+        MatInputModule,
+        MatButtonModule,
+        MatSelectModule,
+        MatDividerModule,
+        MatFormFieldModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+
+    ],
+    exports: [
+        MatListModule,
+        MatTableModule,
+        MatInputModule,
+        MatButtonModule,
+        MatSelectModule,
+        MatDividerModule,
+        MatFormFieldModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+    ]
+})
+export class AngularMaterialModule { }
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,13 +1,35 @@
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+
+
 import { AngularMaterialModule } from "../angular-material/angular-material.module";
+
+import { PipesModule } from "../pipes/pipes.module";
+import { FilterComponent } from './filter/filter.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+
 
 
 @NgModule({
-    declarations:[],
-    imports:[
-        AngularMaterialModule
-    ],
-    exports:[],
-    
+  declarations: [  
+    FilterComponent,
+    UserDetailsComponent,
+    UsersListComponent
+  ],
+  imports: [
+    FormsModule,
+    CommonModule,
+    PipesModule,
+    AngularMaterialModule,
+  ],
+  exports: [
+    FormsModule,
+    FilterComponent,
+    UserDetailsComponent,
+    UsersListComponent
+  ],
+
 })
-export class ComponentsModule{}
+export class ComponentsModule { }
